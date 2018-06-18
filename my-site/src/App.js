60bce231dbdc16e1906728c371b8138518bff595
Liliya Ilyina <lkarpuk@iba.by>
2018-06-18 17:03:52 +0300

    Delete unnecessary components

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
